83665b4e36e918b74bf618ecdff24fe3d6239d31
BoyangDong <bdong.bobo@gmail.com>
2017-07-13 16:48:18 -0500

    New billing info can be inserted into DB

---
+++ budo-billing-app/billing_info_added.php
@@ -0,0 +1,152 @@
+<?php
+	require_once 'config.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Add Billing Info</title>
+</head>
+<body>
+<?php
+
+if(isset($_POST['submit'])){
+	$data_missing = array(); 
+	//firm
+	if(empty($_POST['firm'])){
+		$data_missing[] = 'D';
+	}else{
+		$f = trim($_POST['firm']);
+	}	
+	//office
+	if(empty($_POST['office'])){
+		$data_missing[] = 'UDN';
+	}else{
+		$o = trim($_POST['office']);
+	}
+	//account
+	if(empty($_POST['account'])){
+		$data_missing[] = 'Account';
+	}else{
+		$a = trim($_POST['account']);
+	}	
+	//currency
+	if(empty($_POST['currency'])){
+		$data_missing[] = 'Currency';
+	}else{
+		$c = trim($_POST['currency']);
+	}	
+	//off office
+	if(empty($_POST['off_office'])){
+		$data_missing[] = 'Off_Office';
+	}else{
+		$oo = trim($_POST['off_office']);
+	}	
+	//off account
+	if(empty($_POST['off_account'])){
+		$data_missing[] = 'Off_Account';
+	}else{
+		$oa = trim($_POST['off_account']);
+	}	
+	//description
+	if(empty($_POST['description'])){
+		$data_missing[] = 'Description';
+	}else{
+		$d = trim($_POST['description']);
+	}	
+	//net amount
+	if(empty($_POST['net_amount'])){
+		$data_missing[] = 'Net_Amount';
+	}else{
+		$na = trim($_POST['net_amount']);
+	}	
+	//comment code
+	if(empty($_POST['comment_code'])){
+		$data_missing[] = 'Comment_Code';
+	}else{
+		$cc = trim($_POST['comment_code']);
+	}	
+	if(empty($data_missing)){
+		#die(var_dump(ini_get('include_path')));
+
+
+		$sql = "INSERT INTO billing_info(Firm, Office, Account, Currency, Off_Office, Off_Account, Description, `Net_Amount`, Comment_Code) 
+					VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+/*
+		$stmt = mysqli_prepare($db, $query);
+
+		mysqli_stmt_bind_param($stmt, "sssssssds", $f, $o, $a, $c, $oo, $oa, $d, $na, $cc);
+		# i: Integers; d: Doubles; b: Blobs; s: Everything else
+
+		mysqli_stmt_execute($stmt);
+
+		$affected_rows = mysqli_stmt_affected_rows($stmt);
+*/
+		$stmt = $db->prepare($sql);
+		$insert = $stmt->execute([$f, $o, $a, $c, $oo, $oa, $d, $na, $cc]);
+
+
+		if(count($insert)){
+			echo 'Billing Info Entered';
+			$db = null;
+			//mysqli_stmt_close($stmt);
+			//mysqli_close($db);
+		}else{
+			echo 'Error Occured <br />';
+			//echo mysqli_error($db); 
+		}
+	}else{
+		echo 'Enter the missing data below <br />';
+		foreach($data_missing as $missing){
+			echo "missinig <br />" . $missing;
+		}
+
+	}
+}
+?>
+	<form action="http://localhost:8080/budo-billing-app/billing_info_added.php" method="post">
+	<table border="0">
+		<tr>
+			<td>Firm</td>
+			<td align="center"><input type="text" name="firm" size="30" value="D" maxlength="1"/></td>
+		</tr>
+		<tr>
+			<td>Office</td>
+			<td align="center"><input type="text" name="office" size="30" value="UDN" maxlength="3"/></td>
+		</tr>
+		<tr>
+			<td>Account</td>
+			<td align="center"><input type="text" name="account" size="30"/></td>
+		</tr>
+		<tr>
+			<td>Currency</td>
+			<td align="center"><input type="text" name="currency" size="30" value="U1" maxlength="2"/></td>
+		</tr>
+		<tr>
+			<td>Off Office</td>
+			<td align="center"><input type="text" name="off_office" size="30" value="UDN" maxlength="3"/></td>
+		</tr>
+		<tr>
+			<td>Off Account</td>
+			<td align="center"><input type="text" name="off_account" size="30"/></td>
+		</tr>
+		<tr>
+			<td>Description</td>
+			<td align="center"><input type="text" name="description" size="30"/></td>
+		</tr>
+		<tr>
+			<td>Net Amount</td>
+			<td align="center"><input type="number" name="net_amount" size="30"/></td>
+		</tr>
+		<tr>
+			<td>Comment Code</td>
+			<td align="center"><input type="text" name="comment_code" size="30" value="R" maxlength="1"/></td>
+		</tr>
+
+		<tr> 
+		<td colspan="2" align="center"><input type="submit" name="submit" value="Send"/></td>
+		</tr>
+	</table>
+	</form>
+</body>
+</html>>
--- budo-billing-app/mysql_connect.php
+++ budo-billing-app/mysql_connect.php
@@ -1,54 +1,57 @@
-<?php
-//Step1
-	$db = mysqli_connect('localhost','root','bdong','mydb')
-	or die('Error connecting to MySQL server.');
-?>
 <html>
-	<head>
-	</head>
- 	<body>
- 	<h1>Budo Billing App</h1>
- 	<?php
-	//Step2
-		#$query = "SELECT * FROM billing_info";
-		$query = "SELECT Firm, Office, Account, Currency, Off_Office, Off_Account, Description, Net_Amount, Comment_Code FROM mydb.billing_info;";
-		mysqli_query($db, $query) or die('Error querying database.');
+<head>
+<?php
+require_once "config.php";
 
-		$response = mysqli_query($db, $query);		
-		if($response){
-			echo '<table align="left" cellspacing="5" cellpadding="8">
-			<tr>
-				<td align="left"><b>Firm</b></td>
-				<td align="left"><b>Office</b></td>
-				<td align="left"><b>Account</b></td>
-				<td align="left"><b>Currency</b></td>
-				<td align="left"><b>Off_Office</b></td>
-				<td align="left"><b>Off_Account</b></td>
-				<td align="left"><b>Description</b></td>
-				<td align="left"><b>Net_Amount</b></td>
-				<td align="left"><b>Comment_Code</b></td>
-			</tr>';
+#$db = mysqli_connect('localhost','root','bdong','mydb')
+#	or die('Error connecting to MySQL server.');
+?>
+</head>
+<body>
+<h1>Budo Billing App</h1>
+	<?php
+//Step2
+	#$query = "SELECT * FROM billing_info";
+	$query = "SELECT Firm, Office, Account, Currency, Off_Office, Off_Account, Description, Net_Amount, Comment_Code FROM mydb.billing_info;";
+	#mysqli_query($db, $query) or die('Error querying database.');
+	$stmt = $db->query($query);
 
-			while($row = mysqli_fetch_array($response)){
-				echo '<tr><td align="left">' . 
-					$row['Firm'] . '</td><td align="left">' .
-					$row['Office'] . '</td><td align="left">' .
-					$row['Account'] . '</td><td align="left">' . 
-					$row['Currency'] . '</td><td align="left">' .
-					$row['Off_Office'] . '</td><td align="left">' .
-					$row['Off_Account'] . '</td><td align="left">' .
-					$row['Description'] . '</td><td align="left">' .
-					$row['Net_Amount'] . '</td><td align="left">' .
-					$row['Comment_Code'] . '</td><td align="left">' ;
+	#$response = mysqli_query($db, $query);		
+	if(count($stmt) > 0){
+		echo '<table align="left" cellspacing="5" cellpadding="8">
+		<tr>
+			<td align="left"><b>Firm</b></td>
+			<td align="left"><b>Office</b></td>
+			<td align="left"><b>Account</b></td>
+			<td align="left"><b>Currency</b></td>
+			<td align="left"><b>Off_Office</b></td>
+			<td align="left"><b>Off_Account</b></td>
+			<td align="left"><b>Description</b></td>
+			<td align="left"><b>Net_Amount</b></td>
+			<td align="left"><b>Comment_Code</b></td>
+		</tr>';
 
+		//while($row = mysqli_fetch_array($response)){
+		while($row = $stmt->fetch()){
+			echo '<tr><td align="left">' . 
+				$row['Firm'] . '</td><td align="left">' .
+				$row['Office'] . '</td><td align="left">' .
+				$row['Account'] . '</td><td align="left">' . 
+				$row['Currency'] . '</td><td align="left">' .
+				$row['Off_Office'] . '</td><td align="left">' .
+				$row['Off_Account'] . '</td><td align="left">' .
+				$row['Description'] . '</td><td align="left">' .
+				$row['Net_Amount'] . '</td><td align="left">' .
+				$row['Comment_Code'] . '</td><td align="left">' ;
 				echo '</tr>';
-			}
-			echo '</table>';
-		}else{
-			echo "Couldn't issue database query";
-			echo mysqli_error($db); 
 		}
-		mysqli_close($db);		
+		echo '</table>';
+	}else{
+		echo "Couldn't issue database query";
+		//echo mysqli_error($db); 
+	}
+	//mysqli_close($db);		
+	$db = null;
 	?>
-	</body>
+</body>
 </html>
