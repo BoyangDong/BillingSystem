6397723c091f872c35cad9e09dba0ecb3d90eaf9
BoyangDong <bdong.bobo@gmail.com>
2017-11-05 17:51:22 -0600

    .xlsx export function added

--- budo-billing-app/api/export.php
+++ budo-billing-app/api/export.php
@@ -1,11 +1,54 @@
 <?php
 require 'db_config.php';
+require '../XLSXWriter/xlsxwriter.class.php';
 
 $today = new DateTime();
 $date = $today->format('Y_m_d');
 $filename = $date . '_EDF_Budo_Rebills'; 
 
-$attachment_config = 'Content-Disposition: attachment; filename='. $filename .'.csv';
+header('Content-disposition: attachment; filename="'.XLSXWriter::sanitize_filename($filename).'.xlsx"');
+header("Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+header('Content-Transfer-Encoding: binary');
+header('Cache-Control: must-revalidate');
+header('Pragma: public');  
+// I am using pdo for my db connection so I can't use mysql_query to query the databse, and the method has now been
+// removed in php 7 so it's not even advisable to use it anymore.  -- 11/5/17 Bo
+$query = $db->prepare("SELECT * FROM billing_info ORDER BY id DESC");
+$query->execute();
+$results = $query->fetchAll(); 
+//$rows = mysql_fetch_assoc($result); 
+$header = array(
+	'Firm'=>'string',
+	'Office'=>'string',
+	'Account'=>'string',
+	'Currency'=>'string',
+	'Off Office'=>'string',
+	'Off Account'=>'string',
+	'Description'=>'string',
+	'Net Amount'=>'price',
+	'Comment Code'=>'string',
+);
+
+$columnheadersinDb = array('Firm', 'Office', 'Account', 'Currency', 'Off_Office', 'Off_Account', 'Description', 'Net_Amount', 'Comment_Code'); 
+
+$writer = new XLSXWriter();
+$writer->writeSheetHeader('Sheet1', $header);
+foreach($results as $eachRow) {
+	$array = array();
+	foreach($columnheadersinDb as $column) {
+		array_push($array, $eachRow[$column]);
+	}
+	$writer->writeSheetRow('Sheet1', $array);
+}
+
+//$writer->writeSheet($array,'Sheet1', $header);//or write the whole sheet in 1 call    
+$writer->writeToStdOut();
+//$writer->writeToFile('example.xlsx');
+//echo $writer->writeToString();
+exit(0);
+
+// This is how the .csv file is made -- 11/5/17 Bo
+/*$attachment_config = 'Content-Disposition: attachment; filename='. $filename .'.csv';
 
 if(isset($_POST["export"]))
 {
@@ -19,9 +62,6 @@ if(isset($_POST["export"]))
 	foreach ($db -> query("SELECT Firm, Office, Account, Currency, Off_Office, Off_Account, Description, cast(Net_Amount AS CHAR), Comment_Code FROM billing_info") as $row) {
 		fputcsv($output, $row);
 	}
-}
-
-
-
+}*/
 
 ?>
